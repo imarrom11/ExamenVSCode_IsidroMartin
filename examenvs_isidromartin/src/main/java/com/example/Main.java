0067869ba07c4ba099081bee9aa55f57aecb8a1f
Isidro Martín <imarrom@g.educaand.es>
2024-12-04 09:48:30 +0100

    Funcion de multiplicacion y division

--- examenvs_isidromartin/src/main/java/com/example/Main.java
+++ examenvs_isidromartin/src/main/java/com/example/Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner lectura     = new Scanner(System.in);
-        int valor1          = -1;
-        int valor2          = -1;
+        double valor1          = -1;
+        double valor2          = -1;
         String operacion    = "null";
         boolean exit        = false;
 
@@ -14,11 +14,11 @@ public class Main {
         System.out.println("------------------");
 
         do{
-            System.out.print("Introduce la operacion(suma, resta, salir): ");
+            System.out.print("Introduce la operacion(+, -, *, /, salir): ");
             operacion = lectura.nextLine();
 
             switch(operacion){
-                case "suma":
+                case "+":
                     System.out.print("Introduce el primer valor: ");
                     valor1 = Integer.parseInt(lectura.nextLine());
 
@@ -27,7 +27,7 @@ public class Main {
 
                     System.out.println("El resultado de " + valor1 + " más " + valor2 + " es: " + (valor1+valor2));
                     break;
-                case "resta":
+                case "-":
                     System.out.print("Introduce el primer valor: ");
                     valor1 = Integer.parseInt(lectura.nextLine());
 
@@ -35,6 +35,24 @@ public class Main {
                     valor2 = Integer.parseInt(lectura.nextLine());
 
                     System.out.println("El resultado de " + valor1 + " menos " + valor2 + " es: " + (valor1-valor2));
+                    break;
+                case "*":
+                    System.out.print("Introduce el primer valor: ");
+                    valor1 = Integer.parseInt(lectura.nextLine());
+
+                    System.out.print("Introduce el segundo valor: ");
+                    valor2 = Integer.parseInt(lectura.nextLine());
+
+                    System.out.println("El resultado de " + valor1 + " por " + valor2 + " es: " + (valor1*valor2));
+                    break;
+                case "/":
+                    System.out.print("Introduce el primer valor: ");
+                    valor1 = Integer.parseInt(lectura.nextLine());
+
+                    System.out.print("Introduce el segundo valor: ");
+                    valor2 = Integer.parseInt(lectura.nextLine());
+
+                    System.out.println("El resultado de " + valor1 + " entre " + valor2 + " es: " + (valor1/valor2));
                     break;
                 case "salir":
                     exit = true;
